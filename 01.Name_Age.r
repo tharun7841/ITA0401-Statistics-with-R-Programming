94a2c2038cfa447ab8a7dbc92c78fec2f6627a6b
tharun7841 <112371398+tharun7841@users.noreply.github.com>
2023-08-09 14:05:27 +0530

    Create 01.Name_Age.r

---
+++ 01.Name_Age.r
@@ -0,0 +1,5 @@
+name <- readline("Enter your name: ")
+age <- as.integer(readline("Enter your age: "))
+cat("Your name is:", name, "\n")
+cat("Your age is:", age, "\n")
+cat("R version:", R.version$version.string, "\n")
